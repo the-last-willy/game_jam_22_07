58def0dc3b4f75b268a717d2c4241568685ae3bd
Ajanperd <ajanperd@gmail.com>
2022-07-09 23:06:14 +0200

    Fix end game

--- Game/GameManager/GameManager.gd
+++ Game/GameManager/GameManager.gd
@@ -12,11 +12,17 @@ func _process(delta):
 	update_global_anger(delta)
 	update_global_lift(delta)
 
+func clear_ui_instances():
+	game_ui_instance = null
+	game_end_instance = null
+
 func game_over():
 	game_end_instance.game_end(false)
+	clear_ui_instances()
 
 func win():
 	game_end_instance.game_end(true)
+	clear_ui_instances()
 
 func update_global_anger(delta):
 	if(game_ui_instance == null):
