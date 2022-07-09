4318a66e74c0e05e1e86b6db6de146950ba0a499
Willy Jacquet <willy.jacquet@outlook.com>
2022-07-09 19:45:33 +0200

    Luggage variation

--- Game/Props/Luggage/Luggage.gd
+++ Game/Props/Luggage/Luggage.gd
@@ -1,18 +1,15 @@
 extends Spatial
 
-export(PackedScene) var model
-
-var LuggageSettings = load("res://Game/Prop/Luggage/LuggageSettings.gd")
+export(PackedScene) var luggage_configuration;
 
 class_name Luggage
 
 var settings : Dictionary;
 
 func _ready():
-	settings = LuggageSettings.random_luggage_settings()
-	var model_scene = load("res://Game/Prop/Luggage/Model/LuggageModelHeavy.tscn")
-	assert(model_scene != null)
-	add_child(model.instance())
+	settings = luggage_configuration.instance().random_luggage_settings()
+	var model_scene = load(settings.model_path)
+	add_child(model_scene.instance())
 
 func get_handle_position():
 	pass
