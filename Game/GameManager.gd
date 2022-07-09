28fe05985fe3ee2fb486addf653cbb65c9a39c07
Ajanperd <ajanperd@gmail.com>
2022-07-09 17:24:30 +0200

    Added game end screen

--- Game/GameManager.gd
+++ Game/GameManager.gd
@@ -1,4 +1,7 @@
 extends Node
 
 func game_over():
-	pass
+	get_tree().get_node("GameEndUI").game_end(false)
+
+func win():
+	get_tree().get_node("GameEndUI").game_end(true)
---
+++ Game/UI/GameEnd/GameEndUI.gd
@@ -0,0 +1,20 @@
+extends Panel
+
+var main_menu = load("res://Game/Scenes/Main/Main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+func game_end(win : bool):
+	visible = true
+	get_tree().paused = true
+	if win:
+		$VBoxContainer/Label.text = "You win"
+	else:
+		$VBoxContainer/Label.text = "You lose"
+	$VBoxContainer/Exit.grab_focus()
+
+func exit_to_main_menu():
+	get_tree().paused = false
+	var _error_code = get_tree().change_scene_to(main_menu)
