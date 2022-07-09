4f17f20f671eb2d57d0195803e6bb3f88e7ae68c
Garmogg <103140870+Garmogg@users.noreply.github.com>
2022-07-09 14:55:14 +0200

    Collisions asjusted and add control mode

--- Game/Character/Player/Player.gd
+++ Game/Character/Player/Player.gd
@@ -1,5 +1,12 @@
 extends KinematicBody
 
+const CONTROLLER_DEADZONE = 0.2
+
+const CONTROL_MODE_KEYBOARD : int = 0
+const CONTROL_MODE_CONTROLLER : int = 1
+
+var control_mode := CONTROL_MODE_KEYBOARD
+
 var move_speed : float = 3.0
 
 var velocity : Vector3 = Vector3.ZERO
@@ -10,12 +17,13 @@ func _physics_process(delta):
 	
 	var input_dir = Input.get_vector(
 		"move_left", "move_right", 
-		"move_backward", "move_forward"
+		"move_backward", "move_forward",
+		CONTROLLER_DEADZONE
 	)
 	
 	var move_dir : Vector3
 	
-	if input_camera_space:
+	if control_mode == CONTROL_MODE_CONTROLLER:
 		var cam_forward : Vector3 = -get_viewport().get_camera().global_transform.basis.z
 		var cam_right : Vector3 = get_viewport().get_camera().global_transform.basis.x
 		move_dir = input_dir.x * cam_right + input_dir.y * cam_forward
@@ -28,3 +36,10 @@ func _physics_process(delta):
 	if !move_dir.is_equal_approx(Vector3.ZERO):
 		velocity = move_and_slide(move_dir * move_speed)
 		look_at(global_transform.origin + move_dir, Vector3.UP)
+
+func _input(event : InputEvent) -> void:
+	if ((event is InputEventJoypadButton) or 
+		(event is InputEventJoypadMotion and event.axis_value > CONTROLLER_DEADZONE)):
+		control_mode = CONTROL_MODE_CONTROLLER
+	elif (event is InputEventKey) or (event is InputEventMouseButton):
+		control_mode = CONTROL_MODE_KEYBOARD
