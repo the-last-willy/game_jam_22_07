12274f2c12095f7fea6536815cc7c911a77976bf
mikado <mikel15061996@gmail.com>
2022-07-10 14:39:53 +0200

    "gestion des events"

--- Game/EventManager/EventManager.gd
+++ Game/EventManager/EventManager.gd
@@ -17,9 +17,9 @@ var current_events = Events.new().events
 func _ready():
 	var first_event : Dictionary = {}
 	first_event.description = "The plane is currently experiencing a fuel loss.\nPlease regain your seats and keep calm."
-	first_event.lift = -10
-	first_event.anger = 5
-	first_event.fear = 20
+	first_event.lift = -3
+	first_event.anger = 0.05
+	first_event.fear = 0.1
 	
 	event_to_be_played = first_event
 	event_current_delta = 3
@@ -36,6 +36,7 @@ func trigger_current_event():
 	$DialogUI.start_reading(event_to_be_played.description)
 	$Sound.play()
 	GameManager.update_passengers_with_event(event_to_be_played)
+	get_parent().get_node("ProgressionManager").update_load(event_to_be_played.lift)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
